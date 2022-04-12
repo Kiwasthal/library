935141af2d59b505f8bb64057b8824689191b8cc
Steerpike <imthekoul@gmail.com>
2022-04-13 01:25:22 +0300

    created branch to rework project with classes

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
 
 let myLibrary = [];
-let i=0
-let container = document.querySelector('.container')
-let cardContainer = document.getElementById('card-container')
+let i=0;
+let container = document.querySelector('.container');
+let cardContainer = document.getElementById('card-container');
 
 function CreateBook(title,author,pages){
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.read = "Read"
+    this.read = true;
     this.toggleRead = function(){
-        this.read === "Read" ? this.read = "Not Read" : this.read = "Read"
+        this.read ? this.read = false : this.read = true;
     }
 }
 
 function addBookToLibrary(title,author,pages) {
+
     const myBook = new CreateBook(title,author,pages);
     myLibrary.push(myBook);
 }
 
 
 function displayBooks(array){
-  let cardContainer = document.getElementById('card-container')
+  let cardContainer = document.getElementById('card-container');
       let card = document.createElement('div');
       
       let cardTitle = document.createElement('div');
@@ -37,25 +38,26 @@ function displayBooks(array){
       card.appendChild(cardPages);
 
       let btnRead = document.createElement('button');
-      btnRead.textContent = array[i].read
+      btnRead.dataset.index = i;
+      array[i].read ? btnRead.textContent = "Read" : btnRead.textContent = "Not Read";
 
       btnRead.addEventListener('click', function(){
-          array[i-1].toggleRead()
-          btnRead.textContent = array[i-1].read
+        array[btnRead.dataset.index].toggleRead();
+        array[btnRead.dataset.index].read ? btnRead.textContent = "Read" : btnRead.textContent = "Not Read";
       })
 
-      card.appendChild(btnRead)
+      card.appendChild(btnRead);
 
       //Remove listener 
 
       let btnDelete = document.createElement('button');
       btnDelete.addEventListener('click',function(){
-          cardContainer.removeChild(card)
+          cardContainer.removeChild(card);
       })
 
-      btnDelete.classList.add('delete')
-      btnDelete.textContent = "X"
-      card.appendChild(btnDelete)
+      btnDelete.classList.add('delete');
+      btnDelete.textContent = "X";
+      card.appendChild(btnDelete);
         
       card.classList.add('card');
       cardContainer.appendChild(card);
@@ -66,22 +68,22 @@ let addBtn = document.getElementById('add-book');
 //Create form with button press
 
 addBtn.addEventListener('click', function(){
-    document.querySelector('.bg-modal').style.display ='flex' 
+    document.querySelector('.bg-modal').style.display ='flex';
 });
 
 document.querySelector('.close').addEventListener('click',function(){
-    document.querySelector('.bg-modal').style.display ='none'
+    document.querySelector('.bg-modal').style.display ='none';
 })
 
 document.getElementById('create-book').addEventListener('click', function(){
     addBookToLibrary(document.getElementById('book-title').value,
-    document.getElementById('book-author').value,
-    document.getElementById('book-pages').value);
-    displayBooks(myLibrary)
-    ++i
-    document.querySelector('.bg-modal').style.display ='none'
-    document.getElementById('book-title').value = ""
-    document.getElementById('book-author').value = ""
-    document.getElementById('book-pages').value = ""
+                        document.getElementById('book-author').value,
+                            document.getElementById('book-pages').value);
+    displayBooks(myLibrary);
+    ++i;
+    document.querySelector('.bg-modal').style.display ='none';
+    document.getElementById('book-title').value = "";
+    document.getElementById('book-author').value = "";
+    document.getElementById('book-pages').value = "";
 })
 
